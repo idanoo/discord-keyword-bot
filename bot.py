2fd99c369c796bbacfbe71c0723f3b46de748808
Daniel Mason <daniel@m2.nz>
2020-03-21 15:42:08 +1300

    Changes

--- bot.py
+++ bot.py
@@ -12,13 +12,8 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.channel.id == int(os.getenv("SOURCE_CHANNEL")):
             if re.match(os.getenv("REGEX_MATCH"), message.content, re.IGNORECASE):
-                chan_list = [x.strip() for x in os.getenv("DISCORD_CHANNELS").split(',')]
-                for channel_id in chan_list:
-                    channel = client.get_channel(int(channel_id))
-                    ping_string = ''
-                    if os.getenv("PING_ROLE") != '':
-                        ping_string = '<@&' + os.getenv("PING_ROLE") + '>: '    
-                    await channel.send(ping_string + message.content)
+                channel = client.get_channel(int(os.getenv("DISCORD_CHANNEL"))
+                await channel.send(message.content)
 
 load_dotenv()
 client = MyClient()
